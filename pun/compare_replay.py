f296ce11a77d92e300880e982386e0680d8e71b6
Puneet Nagar <45456578+puneater@users.noreply.github.com>
2021-01-28 18:50:31 +0530

    updated for cifar10

--- pun/compare_replay.py
+++ pun/compare_replay.py
@@ -19,7 +19,7 @@ parser.add_argument('--results-dir', type=str, default='./results', dest='r_dir'
 
 # expirimental task parameters.
 task_params = parser.add_argument_group('Task Parameters')
-task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['permMNIST', 'splitMNIST'])
+task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['permMNIST', 'splitMNIST', 'cifar10'])
 task_params.add_argument('--scenario', type=str, default='task', choices=['task', 'domain', 'class'])
 task_params.add_argument('--tasks', type=int, help='number of tasks')
 
@@ -78,6 +78,7 @@ args = parser.parse_args()
 ## Memory budget values to compare
 budget_list_permMNIST = [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
 budget_list_splitMNIST = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+budget_list_cifar10 = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
 
 
@@ -134,7 +135,7 @@ if __name__ == '__main__':
         os.mkdir(args.p_dir)
 
     ## Select correct memory budget list
-    budget_list = budget_list_permMNIST if args.experiment=="permMNIST" else budget_list_splitMNIST
+    budget_list = budget_list_permMNIST if args.experiment=="permMNIST" else budget_list_splitMNIST  # splitMNIST is equal to cifar10
 
     ## Add non-optional input argument that will be the same for all runs
     args.ewc = False
