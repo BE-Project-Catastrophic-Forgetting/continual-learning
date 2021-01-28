72ca6415cc63784678ee8869f331a0a2f2515d24
Puneet Nagar <45456578+puneater@users.noreply.github.com>
2021-01-28 18:56:10 +0530

    updated for cifar10

--- pun/compare_all.py
+++ pun/compare_all.py
@@ -9,7 +9,7 @@ import utils
 from param_values import set_default_values
 
 
-description = 'Compare CL strategies using various metrics on each scenario of permuted or split MNIST.'
+description = 'Compare CL strategies using various metrics on each scenario of permuted, split MNIST or cifar10.'
 parser = argparse.ArgumentParser('./compare_all.py', description=description)
 parser.add_argument('--seed', type=int, default=1, help='[first] random seed (for each random-module used)')
 parser.add_argument('--n-seeds', type=int, default=1, help='how often to repeat?')
@@ -20,7 +20,7 @@ parser.add_argument('--results-dir', type=str, default='./results', dest='r_dir'
 
 # expirimental task parameters.
 task_params = parser.add_argument_group('Task Parameters')
-task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['permMNIST', 'splitMNIST'])
+task_params.add_argument('--experiment', type=str, default='splitMNIST', choices=['permMNIST', 'splitMNIST', 'cifar10'])
 task_params.add_argument('--scenario', type=str, default='task', choices=['task', 'domain', 'class'])
 task_params.add_argument('--tasks', type=int, help='number of tasks')
 
